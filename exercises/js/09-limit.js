454126c46f512108292e00ce4df72a747d07c2ba
Script Raccoon <scriptraccoon@gmail.com>
2025-11-07 23:19:53 +0100

    add exercise on limit decorator

---
+++ exercises/js/09-limit.js
@@ -0,0 +1,61 @@
+/**
+ * Aufgabe: Implementiere eine Dekorator-Funktion, die eine Funktion so abändert, dass
+ * sie innerhalb eines vorgegebenen Zeitintervalls nur einmal ausgeführt werden kann.
+ * Alle zusätzlichen Aufrufe werden verzögert ausgeführt (das ist der Unterschied zu throttle).
+ *
+ * Genauer gesagt, entwickle eine Funktion limit(fn, interval), die eine Funktion
+ * fn entgegennimmt sowie ein Zeitintervall (in Millisekunden) und eine Funktion zurückgibt,
+ * welche fn maximal einmal pro Intervall ausführt. Wenn innerhalb des Intervalls die Funktion
+ * trotzdem aufgerufen wird, wird dieser Aufruf später ausgeführt.
+ *
+ * Beispiel:
+ *
+ * const log_date = (msg: string) => console.info(new Date().toLocaleString("DE-de"), msg)
+ * const log_date_limited = limit(log_date, 1000)
+ *
+ * log_date_limited("a")
+ * log_date_limited("b")
+ * log_date_limited("c")
+ * log_date_limited("d")
+ *
+ * Dann soll das Datum vier mal im Abstand von einer Sekunde geloggt werden.
+ *
+ * Themen: Closure, Dekorator-Funktion, Scheduling
+ */
+
+/**
+ * Dekorator-Funktion, die eine Funktion innerhalb eines Intervals auf nur eine
+ * Ausführung beschränkt und weitere Aufrufe später ausführt.
+ */
+export function limit(fn, interval) {
+	const queue = []
+	let running = false
+
+	function run() {
+		if (queue.length === 0) {
+			running = false
+			return
+		}
+
+		running = true
+
+		const args = queue.shift()
+		fn(...args)
+
+		setTimeout(run, interval)
+	}
+
+	return (...args) => {
+		queue.push(args)
+		if (!running) run()
+	}
+}
+
+// TESTING
+const log_date = (msg) => console.info(new Date().toLocaleString("DE-de"), msg)
+const log_date_limited = limit(log_date, 1000)
+
+log_date_limited("a")
+log_date_limited("b")
+log_date_limited("c")
+log_date_limited("d")
