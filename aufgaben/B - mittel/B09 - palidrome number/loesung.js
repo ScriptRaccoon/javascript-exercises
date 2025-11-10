896df7d01d2f6e902b8064d0d32e85e580c0033a
Script Raccoon <scriptraccoon@gmail.com>
2025-11-10 19:12:01 +0100

    add exercise on palindrome numbers

---
+++ aufgaben/B - mittel/B09 - palidrome number/loesung.js
@@ -0,0 +1,68 @@
+/**
+ * Prüft ob ein String ein Palindrom ist.
+ */
+function is_palindrome(str) {
+	const n = str.length
+	if (str.length === 0) return true
+	for (let i = 0; i < Math.floor(n / 2); i++) {
+		if (str[i] !== str[n - 1 - i]) return false
+	}
+	return true
+}
+
+/**
+ * Bestimmt die kleinste Palindromzahl >= n.
+ * Brute-Force-Algorithmus.
+ */
+function next_palindrome_slow(n) {
+	const is_valid = Number.isInteger(n) && n >= 0
+	if (!is_valid) throw new Error("Number must be a non-negative integer")
+	let k = n
+	while (!is_palindrome(k.toString())) k++
+	return k
+}
+
+/**
+ * Hilfsfunktion, die einen String umkehrt.
+ */
+
+function reverse_string(s) {
+	return [...s].reverse().join("")
+}
+
+/**
+ * Hilfsfunktion, die einen Teilstring umdreht und hinten anhängt.
+ */
+
+function mirrored(left, len) {
+	return left + reverse_string(len % 2 === 0 ? left : left.slice(0, -1))
+}
+
+/**
+ * Bestimmt die kleinste Palindromzahl >= n.
+ * Schneller Algorithmus.
+ */
+function next_palindrome(n) {
+	const is_valid = Number.isInteger(n) && n >= 0
+	if (!is_valid) throw new Error("Number must be a non-negative integer")
+
+	const s = String(n)
+	const len = s.length
+
+	const left = s.slice(0, Math.ceil(len / 2))
+	const mirror = Number(mirrored(left, len))
+
+	if (mirror >= n) return mirror
+
+	const left_add_one = String(Number(left) + 1)
+	return Number(mirrored(left_add_one, len))
+}
+
+/* ------ TESTS ------ */
+
+console.info(next_palindrome(99) == 99)
+console.info(next_palindrome(123) == 131)
+console.info(next_palindrome(9190) == 9229)
+console.info(next_palindrome(10004) == 10101)
+console.info(next_palindrome(1129404) == 1130311)
+console.info(next_palindrome(4249812309120352) == 4249812332189424)
