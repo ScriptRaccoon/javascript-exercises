3d6a46367bbac6f9cc747841a0ef5eac7f13773a
Script Raccoon <scriptraccoon@gmail.com>
2025-12-05 12:28:16 +0100

    add tic tac toe exercise

---
+++ aufgaben/F - komplex/F09 - Eindimensionales Tic-Tac-Toe/README.md
@@ -0,0 +1,61 @@
+# Eindimensionales Tic-Tac-Toe
+
+## Kontext
+
+Diese Aufgabe handelt von einem kombinatorischen Spiel, einer Art eindimensionalem Tic-Tac-Toe.
+
+Auf einem Spielfeld der Größe $n \times 1$ werden von zwei Spielern abwechselnd Kreuze gesetzt. Wer als erstes drei Kreuze nebeneinander gesetzt hat, gewinnt das Spiel. Wer nicht mehr ziehen kann, verliert. In diesem Spiel für $n = 6$ gewinnt zum Beispiel der zweite Spieler:
+
+| Zug | Feld          | Bemerkung                   |
+| --- | ------------- | --------------------------- |
+| 0   | `- - - - - -` | Start                       |
+| 1   | `X - - - - -` | Spieler 1 zieht             |
+| 2   | `X - - X - -` | Spieler 2 zieht             |
+| 3   | `X - - X - X` | Spieler 1 zieht             |
+| 4   | `X - - X X X` | Spieler 2 zieht und gewinnt |
+
+Tatsächlich kann der zweite Spieler für $n = 5$ immer gewinnen, egal was der erste Spieler zieht. Für $n = 5$ hat aber der erste Spieler eine Gewinnstrategie: er setzt das Kreuz in die Mitte.
+
+Man kann zeigen: Für jedes $n \geq 0$ hat entweder der erste oder der zweite Spieler eine Gewinnstrategie.
+
+## Aufgabe
+
+Implementiere eine Funktion, welche alle $n \leq 100.000$ bestimmt, für die der zweite Spieler in diesem Spiel eine Gewinnstrategie hat, also der erste Spieler verliert.
+
+## Beispiel
+
+**Eingabe**
+
+```js
+get_losing_sizes(100_000)
+```
+
+**Ausgabe**
+
+```text
+[
+     0,    2,    6,   12,   22,    30,
+    32,   44,   54,   64,   76,    86,
+    98,  110,  118,  130,  132,   162,
+   170,  184,  194,  202,  282,   290,
+   302,  356, 1046, 2502, 2752,  2912,
+  3052, 3076, 7250, 7356, 7866, 16168
+]
+```
+
+## Ein Beispiel-Spiel
+
+Hier noch ein Beispiel dafür, wie das Spiel für $n = 10$ ablaufen kann. Hier gewinnt der erste Spieler.
+
+```text
+- - - - - - - - - -
+- X - - - - - - - -     Spieler 1
+- X - - - - X - - -     Spieler 2
+- X - - - - X - - X     Spieler 1
+- X - X - - X - - X     Spieler 2
+- X X X - - X - - X     Spieler 1
+```
+
+## Themen
+
+Spieltheorie, Kombinatorik, Rekursion, Laufzeitoptimierung, Grundy-Wert
---
+++ aufgaben/F - komplex/F09 - Eindimensionales Tic-Tac-Toe/loesung.ts
@@ -0,0 +1,114 @@
+/*
+The game 1-dimensional Tic Tac Toe is also known as Treblecross
+(https://en.wikipedia.org/wiki/Treblecross). It is a combinatorial
+game, and we need to find its P-positions. We need some results of
+combinatorial game theory:
+- Every combinatorial game is equivalent to a Nim game *n, where n
+  is the Grundy value of the game.
+- A game is a P-position if and only if its Grundy value is 0.
+- The Grundy value of a sum of games is the Nim sum of the Grundy values.
+
+On boards >= 3 Treblecross is "equivalent" to a simpler game:
+every 1 also occupies the max. 4 zeros around it:
+
+0 0 1 0 0
+
+These fields can be removed. What remains is a bunch of isolated
+blocks of zeros. What only matters is the tuple of their lengths.
+We get a sum of games, each one with just one pile.
+
+Given a pile with x > 0 numbers, we may:
+
+- remove it when x <= 5
+- replace x with x - 3 (when x > 3)
+- replace x with x - 4 (when x > 4)
+- replace x with x - 5 (when x > 5)
+- replace x by two piles with a,b > 0 numbers where a + b = x - 5
+
+In other words, this is the "take-and-break" game .11337. For the
+notation, see Chapter 4 in the book "Winning Ways" (Vol. 1).
+
+The "equivalence" respects the P-positions, but not the Grundy values.
+Still, the Grundy value of a sum is easy to compute using the Nim sum.
+So even when we are only interested in the P-positions, this approach
+is very fast. In fact, it allows us to compute the P-positions for
+all n <= 100.000 (or even n <= 250.000) quickly.
+
+The authors of "Winning Ways" identify Treblecross with the game .007,
+which however requires to expand the board on both sides by 1 slot.
+This also explains the 2-shift between our values and the ones on
+the website https://wwwhomes.uni-bielefeld.de/achim/octal.html.
+See also https://math.stackexchange.com/q/4283174 for an explanation.
+*/
+
+import { create_lru_cache, mex } from "./utils"
+
+/**
+ * Cache for storing the Grundy value of a heap of a given size.
+ */
+const grundy_cache = create_lru_cache<number, number>(1_000_000, 10_000)
+
+/**
+ * Returns the Grundy value of a Treblecross board (after being simplified
+ * as above) of a size n. The P-positions are those with Grundy value 0.
+ */
+function treblecross_grundy(n: number): number {
+	if (n === 0) return 0
+
+	const cached_value = grundy_cache.get(n)
+	if (cached_value !== undefined) return cached_value
+
+	const options: number[] = []
+
+	if (n <= 5) options.push(0)
+	for (const d of [3, 4, 5]) {
+		if (n > d) options.push(treblecross_grundy(n - d))
+	}
+	for (let a = 1; a <= (n - 5) / 2; a++) {
+		const b = n - 5 - a
+		if (b >= 1) {
+			const s = treblecross_grundy(a) ^ treblecross_grundy(b)
+			options.push(s)
+		}
+	}
+
+	const res = mex(options)
+	grundy_cache.set(n, res)
+	return res
+}
+
+/**
+ * Returns the list of sizes n for which Treblecross of size n
+ * is won by the second player (in optimal play).
+ */
+export function get_losing_sizes(limit: number, print = true): number[] {
+	if (print) console.info("Determine P-positions in Treblecross")
+	const P_positions: number[] = [0, 2]
+
+	for (let n = 3; n <= limit; n++) {
+		if (print && n % 1_000 === 0) console.info("Computing numbers >=", n, "...")
+		const g = treblecross_grundy(n)
+		if (g === 0) {
+			if (print) console.info("👀 Found P-position:", n)
+			P_positions.push(n)
+		}
+	}
+	return P_positions
+}
+
+// Usage:
+// console.info(get_losing_sizes(100_000))
+
+/*
+[
+     0,    2,    6,   12,   22,    30,
+    32,   44,   54,   64,   76,    86,
+    98,  110,  118,  130,  132,   162,
+   170,  184,  194,  202,  282,   290,
+   302,  356, 1046, 2502, 2752,  2912,
+  3052, 3076, 7250, 7356, 7866, 16168
+]
+
+It seems like this is a *finite* set of 36 P-positions. But the proof
+is a hard unsolved problem in combinatorial game theory.
+*/
